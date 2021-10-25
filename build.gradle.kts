a64655876ce2b88c7abc82666e12f8c309087254
Claudio Altamura <claudio@claudioaltamura.de>
2021-10-25 21:25:54 +0200

    chore: added reactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,19 @@ repositories {
 }
 
 dependencies {
-    implementation("io.micrometer:micrometer-core:1.7.3")
+    implementation("io.micrometer:micrometer-core:1.7.5")
 
     implementation("io.micrometer:micrometer-registry-prometheus:1.7.3")
 
-    testImplementation("org.assertj:assertj-core:3.20.2")
+    implementation ("io.projectreactor.netty:reactor-netty-http:1.0.12")
+
+    testImplementation("org.assertj:assertj-core:3.21.0")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
