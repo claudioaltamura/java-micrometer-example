6bc024c61d51e14c4cd29813440dbd2180801450
Claudio Altamura <claudio@claudioaltamura.de>
2021-10-25 21:26:41 +0200

    chg; simple counter example

--- src/main/java/de/claudioaltamura/java/micrometer/Example.java
+++ src/main/java/de/claudioaltamura/java/micrometer/Example.java
@@ -1,16 +1,26 @@
 package de.claudioaltamura.java.micrometer;
 
+import java.time.Duration;
+
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import reactor.core.publisher.Flux;
 
 public class Example {
 
 	public static void main(String[] args) {
-		MeterRegistry registry = SampleRegistry.prometheus();
+		MeterRegistry meterRegistry = SampleRegistry.prometheus();
 
-		final var processor = new XYZProcessor(registry);
-		final var key = "id";
-		final var value = "12345";
-		processor.execute(key, value);
+		var key = "counter";
+		Counter counter = meterRegistry.counter("counter", key, "mycounter");
+
+		Flux.interval(Duration.ofMillis(30))
+				.doOnEach(d -> {
+					int random = (int)(Math.random()*10000);
+					counter.increment(random);
+				})
+				.log()
+				.blockLast();
 	}
 
 
