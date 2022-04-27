f8aee14804661b74af044c5b8eabb79311ce1be9
Claudio Altamura <claudio@claudioaltamura.de>
2022-04-27 16:42:26 +0200

    chore: housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,28 @@
 plugins {
     java
+    id("com.diffplug.spotless") version "6.5.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
+
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.micrometer:micrometer-core:1.7.5")
+    implementation("io.micrometer:micrometer-core:1.8.5")
 
-    implementation("io.micrometer:micrometer-registry-prometheus:1.7.3")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
 
-    implementation ("io.projectreactor.netty:reactor-netty-http:1.0.12")
+    implementation ("io.projectreactor.netty:reactor-netty-http:1.0.18")
 
-    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.test {
